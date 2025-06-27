c70c9e9151326acd182a5dc0921ad1f3c36eabc1
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2025-06-27 08:25:42 -0300

    JavaScript para funcionamento do contador

---
+++ JavaScript/Contador/assets/js/script.js
@@ -0,0 +1,18 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function decrement(){
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber < 0){
+        currentNumberWrapper.style.color = "red";
+    }
+}
+
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber >= 0){
+        currentNumberWrapper.style.color = "black";
+    }
+}
