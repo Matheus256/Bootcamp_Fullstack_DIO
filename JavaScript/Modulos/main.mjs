194637445c6f0e513b9ab6185bdbe84a37046302
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2025-07-01 08:36:29 -0300

    Arquivo principal que vai importar funções

---
+++ JavaScript/Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Matheus", 27));
+console.log(mostraCidade("Matheus","Campina grande"));
+console.log(mostraHobby("Matheus", "dormir"));
