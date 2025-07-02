9ef8fe1d6cf0f0be2d90ff682dfe756318e35703
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2025-07-02 08:49:26 -0300

    Teste do desafio 2-2

---
+++ JavaScript/Praticas/Teste1.js
@@ -0,0 +1,14 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+console.log('---------------------------------------');
+for(let j = 0; j < 4; j++){
+  let linha = '|';
+  for(let i = 0; i < 37; i++){
+    linha = linha + ' ';
+  }
+  linha = linha + '|';
+  console.log(linha);
+}
+console.log('---------------------------------------');
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
