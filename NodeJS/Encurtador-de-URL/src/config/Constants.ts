0661f34efcbeb7e14608586487483a18a157fdf1
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2025-07-18 08:55:54 -0300

    Configurações inicias da porta e do link para o atlas

---
+++ NodeJS/Encurtador-de-URL/src/config/Constants.ts
@@ -0,0 +1,7 @@
+
+//O projeto contou com um banco de dados remoto obtido através do Atlas Database do MongoDB 
+export const config = {
+	API_URL: 'http://localhost:8080', //Porta do localhost usada para fazer as requisições
+	MONGO_CONNECTION:
+		'link_mongodb', //Inserir o link obtido após a criação no Atlas Database
+}
